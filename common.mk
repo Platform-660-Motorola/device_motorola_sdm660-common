b20af99a39e913cc301993905fabe381bd6a9ce5
Vhmit <viktor.9630@hotmail.com>
2023-12-01 23:16:40 -0300

    sdm660-common: Replace Camera AOSP to GcamGO
    Change-Id: I594baf3bd85732183e4974f44fb0f58ce7a9f6b1
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- common.mk
+++ common.mk
@@ -73,6 +73,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    GcamGo \
     libbson.vendor \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0:64
